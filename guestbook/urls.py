887f9c14d810beb71ce5954d4d039743be500e6c
Kairat <ischenbaew@gmai.com>
2024-07-06 12:21:05 +0200

    изменил стили подключил Bootstrap, заменил кнопки на иконки

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_entry, name='add_entry'),
     path('edit/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    path('delete/<int:entry_id>/', views.delete_entry, name='delete_entry'),
 ]
