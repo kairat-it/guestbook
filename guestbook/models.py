c4b3ef3b3a2e5708d80fb171bc3007b1db15e43c
Kairat <ischenbaew@gmai.com>
2024-07-06 10:32:07 +0200

    Создал модель в models.py

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class GuestbookEntry(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Активно'),
+        ('blocked', 'Заблокировано'),
+    ]
+
+    name_author = models.CharField(max_length=100, verbose_name="Имя автора")
+    email_author = models.EmailField(verbose_name="Почта автора")
+    entry_text = models.TextField(verbose_name="Текст записи")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Время редактирования")
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='active',
+        verbose_name="Статус"
+    )
+
+    def __str__(self):
+        return f"{self.name_author} - {self.status}"
